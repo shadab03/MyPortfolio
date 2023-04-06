12a801ebed3481a4f01eeefb7143ce624bf7559b
shadab syed <iam.shadab@outlook.com>
2023-04-06 12:55:59 +0530

    about fixes

--- pages/about.js
+++ pages/about.js
@@ -23,13 +23,13 @@ const About = () => {
           <div className='col-span-3 flex flex-col items-start justify-start'>
             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
             <p className='font-medium'>
-              Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and innovative ways to bring my clients' visions to life.
+              Hi, I&apos;m Shadab, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and innovative ways to bring my clients&apos; visions to life.
             </p>
             <p className='my-4 font-medium'>
-              I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users.
+              I believe that design is about more than just making things look pretty – it&apos;s about solving problems and creating intuitive, enjoyable experiences for users.
             </p>
             <p className='my-4 font-medium'>
-              Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+              Whether I&apos;m working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
             </p>
           </div>
           <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
